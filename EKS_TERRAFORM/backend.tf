c8e1d4cdaa450a94a8292a38ab2d731018292fbd
Prashanth Chowla <103575694+Prashanthchowla1@users.noreply.github.com>
2025-12-06 10:08:19 +0530

    Change S3 bucket name in backend configuration

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "pras-ohio243" # Replace with your actual S3 bucket name
+    bucket = "frst-bucket-pr" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
